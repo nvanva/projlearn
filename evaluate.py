ef707adfb524dc29a0baa28a95028c8a3e5d0c49
Ubuntu <ubuntu@ip-10-234-156-80.us-west-1.compute.internal>
2016-11-30 12:09:08 +0000

    batch_sim module introduced, evalute.py speed up

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from batch_sim.nn_vec import nn_vec
 import argparse
 import csv
 import glob
@@ -96,8 +96,11 @@ for path in args['path']:
 
         if not args['non_optimized']:
             # TODO: normalize Y_all_hat ???
-            similar_indices = nn_vec_basic(Y_all_hat, w2v.syn0norm, topn=10, sort=True, return_sims=False, nthreads=8)
+            print('nn_vec...')
+            similar_indices = nn_vec(Y_all_hat, w2v.syn0norm, topn=10, sort=True, return_sims=False, nthreads=8, verbose=False)
+            print('nn_vec results covert...')
             similar_words = [[w2v.index2word[ind] for ind in row] for row in similar_indices]
+            print('done')
 
         for i, (hyponym, hypernym) in enumerate(subsumptions_test):
             if args['non_optimized']:
