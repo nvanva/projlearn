7daa0009400144deced45a387c82b8bf7b34cd7e
Ubuntu <ubuntu@ip-10-198-77-118.us-west-1.compute.internal>
2017-02-15 08:08:46 +0000

    toyota

--- evaluate.py
+++ evaluate.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 import numpy as np
 
 
-MODELS = ['baseline', 'regularized_hyponym', 'regularized_synonym', 'regularized_hypernym', 'frobenius_loss', 'mlp']
+MODELS = ['baseline', 'regularized_hyponym', 'regularized_synonym', 'regularized_hypernym', 'frobenius_loss', 'mlp','toyota']
 
 parser = argparse.ArgumentParser(description='Evaluation.')
 parser.add_argument('--w2v',          default='all.norm-sz100-w10-cb0-it1-min100.w2v', nargs='?', help='Path to the word2vec model.')
